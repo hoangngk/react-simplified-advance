bb89935fcdb574dfebecf0f4af39c4b79bd039b2
Hoang NK <n.khanhhoang@gmail.com>
2024-07-30 11:19:44 +0700

    clean main

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
 import './App.css'
-import ContactList from './ContactList.tsx'
-import Chat from './Chat.tsx'
-import { useState } from 'react'
-
-const contacts = [
-  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
-  { id: 1, name: 'Alice', email: 'alice@mail.com' },
-  { id: 2, name: 'Bob', email: 'bob@mail.com' },
-]
 
 function App() {
-  const [to, setTo] = useState(contacts[0].email)
-
-  return (
-    <>
-      <ContactList contacts={contacts} onSelect={setTo} />
-      <Chat to={to} key={to} />
-    </>
-  )
+  return <>Hi</>
 }
 
 export default App
